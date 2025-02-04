6cb479e7e3037a426ff4247b50e9cf53e314cd11
Rifat Nabi <to.rifat@gmail.com>
2025-02-04 17:09:11 +1100

    Fix wifi issue

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -19,7 +19,10 @@
     "usbhid"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [
+    "kvm-intel"
+    "iwlwifi"
+  ];
   boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -32,5 +35,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
 }
--- home/hyprland/configuration.nix
+++ home/hyprland/configuration.nix
@@ -3,7 +3,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "firefox"
+      "zen"
       "ghostty"
       "1password --silent"
     ];
