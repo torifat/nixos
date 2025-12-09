4a3f7f34bfc3d1f931e150d0e155184637bffa5b
Rifat Nabi <to.rifat@gmail.com>
2025-12-10 05:15:36 +1100

    POE

--- gaming/poe.nix
+++ gaming/poe.nix
@@ -4,6 +4,7 @@
   # covers both poe1 and poe2
   environment.systemPackages = with pkgs; [
     awakened-poe-trade
+    exiled-exchange-2
     rusty-path-of-building
   ];
 }
--- packages/exiled-exchange-2/default.nix
+++ packages/exiled-exchange-2/default.nix
@@ -1,5 +1,6 @@
 {
   appimageTools,
+  pkgs,
   mySource,
   ...
 }:
@@ -10,16 +11,21 @@ let
 in
 appimageTools.wrapType2 rec {
   inherit (mySource) pname version src;
-
   extraInstallCommands = ''
     install -Dm444 ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace-fail 'Exec=AppRun --sandbox %U' 'Exec=${pname} %U'
-
-    install -m 444 -D ${appimageContents}/awakened-poe-trade.png $out/share/icons/hicolor/128x128/apps/${pname}.png
+    for icon in ${appimageContents}/usr/share/icons/hicolor/*/apps/${pname}.png; do
+      size=$(basename "$(dirname "$icon")")
+      install -Dm444 "$icon" "$out/share/icons/hicolor/$size/apps/${pname}.png"
+    done
   '';
 
-  meta = {
+  extraPkgs = pkgs: [ pkgs.fuse ];
+  meta = with pkgs.lib; {
+    description = "Price checker overlay for Path of Exile 2";
+    homepage = "https://kvan7.github.io/Exiled-Exchange-2";
+    license = licenses.unfreeRedistributable;
     mainProgram = pname;
     platforms = [ "x86_64-linux" ];
   };
